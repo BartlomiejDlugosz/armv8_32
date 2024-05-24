e51892cc05eb09405a2a19aa3dd539697c6d55cb
ishaan <ishaan@DESKTOP-N2FKMMP>
2024-05-24 21:29:05 +0100

    added print statements and fixed writing and reading from memory

--- src/structures.c
+++ src/structures.c
@@ -34,7 +34,7 @@ void write_register32(CPU *cpu, uint8_t reg_index, uint32_t value) {
     }
 }
 
-uint64_t read_memory(const CPU *cpu, uint64_t address) {
+uint32_t read_memory(const CPU *cpu, uint32_t address) {
     if (address < MEMORY_SIZE) {
         return cpu->memory[address];
     } else {
@@ -42,8 +42,28 @@ uint64_t read_memory(const CPU *cpu, uint64_t address) {
     }
 }
 
-void write_memory(CPU *cpu, uint64_t address, uint64_t value) {
+void write_memory(CPU *cpu, uint32_t address, uint32_t value) {
     if (address < MEMORY_SIZE) {
         cpu->memory[address] = value;
+    }
+}
+
+void print_cpu_state(const CPU *cpu) {
+    printf("Registers:\n");
+    for (int i = 0; i < NUM_REGISTERS; i++) {
+        printf("X%02d = %016lx\n", i, cpu->registers[i]);
+    }
+    printf("PC = %016lx\n", cpu->PC);
+    printf("PSTATE: %c%c%c%c\n",
+            cpu->pstate.N ? 'N' : '-',
+            cpu->pstate.Z ? 'Z' : '-',
+            cpu->pstate.C ? 'C' : '-',
+            cpu->pstate.V ? 'V' : '-');
+    printf("Non-zero memory:\n");
+    for (uint32_t addr = 0; addr < MEMORY_SIZE; addr += 4) {
+        uint32_t value = read_memory(cpu, addr);
+        if (value != 0) {
+            printf("0x%08x: 0x%08x\n", addr, value);
+        }
     }
 }
