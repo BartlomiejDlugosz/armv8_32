aba6222d242b35ccf9d0b8dce2de80d41b6eca77
Zeshan Uddin <zu123@ic.ac.uk>
2024-05-24 20:22:19 +0100

    updated to fit structeres header file

--- src/emulate.c
+++ src/emulate.c
@@ -1,34 +1,41 @@
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 int main(int argc, char **argv) {
-  extern int current_instr;
-  extern int pc;
+  extern uint32_t current_instr;
+  extern struct CPU;
   extern branchExecute(int);
   extern dataProcessing(int, bool);
   extern loadStore (int);
+  current_instr = CPU->memory[CPU->PC]
   while (current_instr != 0x8a000000) {
+    current_instr = CPU->memory[CPU->PC]
     switch(current_instr & 0x1e000000) {
       case 0x10000000:
+        // fall thru immediate DP
       case 0x12000000:
         dataProcessing(current_instr, true);
         break;
       case 0xA000000:
+        // fall thru register Dp
       case 0x1A000000:
         dataProcessing(current_instr, false);
         break;
       case 0x8000000:
+        // fall thru loadstore
       case 0x18000000:
       case 0x1C000000:
         loadStore(current_instr);
         break;
       case 0x14000000:
-        // fall thru to op0 = 1011
+        // fall thru to branch execute
       case 0x16000000:
         branchExecute(current_instr);
         break;
     }
-    pc += 4;
+    CPU->PC += 4;
   }
+  
   return EXIT_SUCCESS;
 }
