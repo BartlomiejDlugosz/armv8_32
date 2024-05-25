4f58d9f2d9c90fd6dcc957c91339e9f1e0d74aeb
Bartlomiej Dlugosz <bd523@ic.ac.uk>
2024-05-25 13:44:26 +0100

    Fixed data structures corresponding to the instructions

--- src/single_data_transfer.h
+++ src/single_data_transfer.h
@@ -1,17 +1,22 @@
+#include <stdint.h>
+#include "structures.h"
+
+union single_data_transfer_data_offset {
+    struct {
+        unsigned : 1;
+        unsigned I : 1;
+        signed simm9 : 9;
+        unsigned type : 1;
+    };
+    unsigned bits : 12;
+};
+
 // Defines the inner structure for the instruction which is dependent on whether
 // it's a Single Data Transfer or Load Literal instruction
 union single_data_transfer_data {
     struct {
         unsigned xn : 5;
-        union {
-            struct {
-                unsigned : 1;
-                unsigned I : 1;
-                signed simm9 : 9;
-                unsigned type : 1;
-            };
-            unsigned offset : 12;
-        };
+        unsigned offset : 12;
         unsigned L : 1;
     };
     unsigned bits : 19;
@@ -21,7 +26,7 @@ union single_data_transfer_data {
 union single_data_transfer_instruction {
     struct {
         unsigned rt: 5;
-        union single_data_transfer_data data;
+        unsigned data : 19;
         unsigned U: 1;
         unsigned : 5;
         unsigned sf: 1;
@@ -31,6 +36,6 @@ union single_data_transfer_instruction {
 };
 
 
-void single_data_transfer(CPU *cpu, uint64_t *target_address, union single_data_transfer_instruction instr);
+void single_data_transfer(CPU *cpu, uint64_t *target_address, union single_data_transfer_instruction instr, union single_data_transfer_data data);
 
 void single_data_transfer_init(CPU *cpu, uint32_t instruction);
