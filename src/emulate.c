ffaa7ebaeb02150e1be1d716897958aef3b66c26
Bartlomiej Dlugosz <bd523@ic.ac.uk>
2024-05-25 18:10:10 +0100

    Implemented a function to write the state of the CPU after execution finished

--- src/emulate.c
+++ src/emulate.c
@@ -7,8 +7,8 @@ void readBinaryToMemory(int argc, char **argv, CPU *cpu) {
   FILE *input_file;
 
   // Ensures that there are enough arguments provided
-  if (argc >= 1) {
-    input_file = fopen(argv[0], "rb");
+  if (argc > 1) {
+    input_file = fopen(argv[1], "rb");
     if (input_file == NULL) {
       exit(1);
     }
@@ -22,10 +22,29 @@ void readBinaryToMemory(int argc, char **argv, CPU *cpu) {
   fclose(input_file);
 }
 
+void writeCPUState(int argc, char **argv, CPU *cpu) {
+    if (argc > 2) {
+        // A output file was given as a command line argument
+        FILE *output_file;
+
+        output_file = freopen(argv[2], "w", stdout);
+
+        print_cpu_state(cpu);
+
+        fclose(output_file);
+    } else {
+        print_cpu_state(cpu);
+    }
+}
+
 int main(int argc, char **argv) {
   CPU cpu;
   initialise_cpu(&cpu);
   readBinaryToMemory(argc, argv, &cpu);
+
+  
+
+  writeCPUState(argc, argv, &cpu);
 
   return EXIT_SUCCESS;
 }
