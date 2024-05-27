df5e2f4483de9cb49cf4eb902d0c8ad78cb23bc0
Zeshan Uddin <zu123@ic.ac.uk>
2024-05-27 17:51:55 +0100

    small changes

--- src/emulate.c
+++ src/emulate.c
@@ -9,33 +9,33 @@ int main(int argc, char **argv) {
   extern branchExecute(int);
   extern dataProcessing(int, bool);
   extern loadStore (int);
-  current_instr = read_memory(cpu, cpu->PC)
+  current_instr = read_memory(&cpu, cpu.Pc)
   while (current_instr != 0x8a000000) {
-    current_instr = read_memory(cpu, cpu->PC)
+    current_instr = read_memory(&cpu, cpu.Pc)
     switch(current_instr & 0x1e000000) {
       case 0x10000000:
         // fall thru immediate DP
       case 0x12000000:
-        dataProcessing(current_instr, true);
+        dataProcessing(&cpu, current_instr, true);
         break;
       case 0xA000000:
         // fall thru register Dp
       case 0x1A000000:
-        dataProcessing(current_instr, false);
+        dataProcessing(&cpu, current_instr, false);
         break;
       case 0x8000000:
         // fall thru loadstore
       case 0x18000000:
       case 0x1C000000:
-        loadStore(current_instr);
+        loadStore(current_instr, &cpu);
         break;
       case 0x14000000:
         // fall thru to branch execute
       case 0x16000000:
-        branchExecute(current_instr);
+        branchExecute(current_instr, &cpu);
         break;
     }
-    cpu->PC += 4;
+    cpu.PC += 4;
   }
   
   return EXIT_SUCCESS;
