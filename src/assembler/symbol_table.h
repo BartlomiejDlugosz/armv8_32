31f6e7ead817901f5bb0c43023945f68afd31806
ishaan <ishaan@DESKTOP-N2FKMMP>
2024-06-11 20:14:43 +0100

    symbol table compile fix

--- src/assembler/symbol_table.h
+++ src/assembler/symbol_table.h
@@ -1,3 +1,9 @@
+#ifndef SYMBOL_TABLE_H
+#define SYMBOL_TABLE_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
 typedef struct symbol_entry {
     char *label;
     uint64_t address;
@@ -10,9 +16,11 @@ typedef struct symbol_table {
 } symbol_table;
 
 
-symbol_table *init_symbol_table(size_t initial_size, size_t capacity);
+symbol_table *init_symbol_table(size_t capacity);
 void add_entry(symbol_table *table , char *label, uint64_t address); // automatically resizes if necessary
 uint64_t find_entry(symbol_table *table, char *search_label);
 void free_symbol_table(symbol_table *table); 
 
 static void resize_table(symbol_table* table, size_t capacity_scale_factor);
+
+#endif // SYMBOL_TABLE_H
