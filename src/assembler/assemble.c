ea81aa53061d023b84f44afccbc117b6861ce1dc
ishaan <ishaan@DESKTOP-N2FKMMP>
2024-06-11 12:11:37 +0100

    starting to use parse

--- src/assembler/assemble.c
+++ src/assembler/assemble.c
@@ -1,9 +1,17 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include "symbol_table.h"
 
-
-// void first_pass(FILE* in, SymbolTable* table) {
-
-// }
+void first_pass(FILE* in, SymbolTable* table) {
+    char line[256];
+    uint32_t address = 0;
+    while (fgets(line, sizeof(line), in)) {
+        instruction instr = parse(line);
+        if (strlen(instr.label) > 0) {
+            add_symbol(table, instr.label, address);
+        }
+    }
+}
 
 int main(int argc, char **argv) { 
     return EXIT_SUCCESS; 
