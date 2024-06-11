369975a24e53388fd50e5965df45a4021052566a
Bartlomiej Dlugosz <bd523@ic.ac.uk>
2024-06-11 21:19:59 +0100

    Renammed dynamic array to better suit use case. Also added regex matching for parser

---
+++ src/assembler/parser.h
@@ -0,0 +1,12 @@
+#include "dynamicString.h"
+
+typedef struct {
+  // Dynamic since we don't know size of label
+  dynamicString label;
+  // Will always be a max of 10 characters
+  char opcode[10];
+  // Max number of operands is 4, may include labels with unkown size
+  dynamicString operands[4];
+} instruction;
+
+instruction parse(char *);
