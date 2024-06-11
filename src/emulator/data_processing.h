5473cdcbf47a3931a8b4c879ff08ae7befc6e782
Stefan van Biljon <sv823@gpu35.doc.ic.ac.uk>
2024-06-11 21:38:56 +0100

    refactor to not use union (for undefined behaviour)

--- src/emulator/data_processing.h
+++ src/emulator/data_processing.h
@@ -3,72 +3,51 @@
 
 #include "includes.h"
 
-typedef union {
-    struct {
+typedef struct {
         unsigned rn : 5;
         unsigned imm12 : 12;
         unsigned sh : 1;
-    };
-    unsigned bits : 18;
 } arithmetic_immediate_operand;
 
-typedef union {
-    struct {
+typedef struct {
         unsigned imm16 : 16;
         unsigned hw : 2;
-    };
-    unsigned bits : 18;
 } wide_move_operand;
 
-typedef union {
-    struct {
+typedef struct {
         unsigned operand : 18;
         unsigned opi : 3;
-    };
-    unsigned bits : 21;
 } data_processing_data_immediate;
 
-typedef union {
-    struct {
+typedef struct {
         unsigned ra : 5;
         unsigned x : 1;
-    };
-    unsigned bits : 6;
 } multiply_operand;
 
-typedef union {
-    struct {
+typedef struct {
         unsigned maybe_N : 1;
         unsigned shift : 2;
         unsigned type : 1;
-    };
-    unsigned bits : 4;
 } arithmetic_logic_opr;
 
-typedef union  {
-    struct {
+typedef struct {
         unsigned rn : 5;
         unsigned operand : 6;
         unsigned rm : 5;
         unsigned opr : 4;
-        unsigned const_1 : 1;
-    };
-    unsigned bits : 21;
+        //unsigned const_1 : 1; // not needed as not using union
 } data_processing_data_register;
 
 // Defines typedef union structure for the Single Data Processing instructions. Note
 // that the data field in the struct varies for immediate and register based
 // instructions, as defined above
-typedef union  {
-    struct {
+typedef struct  {
         unsigned rd : 5;
         unsigned data : 21;
-        unsigned : 2;
+        //unsigned : 2; // not needed as not using union
         unsigned maybe_M : 1;
         unsigned opc : 2;
         unsigned sf : 1;
-    };
-    unsigned bits : 32;
 } data_processing_instruction;
 
 // Handle immediate instructions
@@ -133,5 +112,27 @@ void perform_data_processing_register(CPU *cpu,
                                       data_processing_data_register data);
 
 void data_processing_init(CPU *cpu, uint32_t instruction, bool is_immediate);
+
+
+arithmetic_immediate_operand init_arithmetic_immediate_operand(uint32_t operand);
+
+
+wide_move_operand init_wide_move_operand(uint32_t operand);
+
+
+data_processing_data_immediate init_data_processing_data_immediate(uint32_t data);
+
+
+multiply_operand init_multiply_operand(uint32_t operand);
+
+
+arithmetic_logic_opr init_arithmetic_logic_opr(uint32_t opr);
+
+
+data_processing_data_register init_data_processing_data_register(uint32_t data);
+
+
+data_processing_instruction init_data_processing_instruction(uint32_t instruction);
+ 
 
 #endif  // DATA_PROCESSING_H
