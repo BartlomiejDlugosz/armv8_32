3d71f312ba5ef7d072c4335dab337004b8757495
Bartlomiej Dlugosz <bd523@ic.ac.uk>
2024-06-12 02:15:34 +0100

    Ran CLang-format

--- src/assembler/dynamicString.h
+++ src/assembler/dynamicString.h
@@ -2,15 +2,15 @@
 #define DYNAMIC_STRING_H
 
 typedef struct dynamicString {
-  char *data;
-  int current_size;
-  int max_size;
-} *dynamicString;
+    char *data;
+    int current_size;
+    int max_size;
+} * dynamicString;
 
 dynamicString createNewDynamicString(int);
 void resizeArray(dynamicString);
 void addChar(dynamicString, char);
-void addString(dynamicString, char*);
+void addString(dynamicString, char *);
 char getChar(dynamicString, int);
 char *getString(dynamicString);
 void freeDynamicString(dynamicString);
