341dfc79c2e648f2d9abffc97085ffa95c72b08e
Stefan van Biljon <sv823@gpu35.doc.ic.ac.uk>
2024-06-12 13:50:56 +0100

    add ifndef to header

--- src/assembler/encode_instruction.h
+++ src/assembler/encode_instruction.h
@@ -1,6 +1,11 @@
+#ifndef ENCODE_INSTRUCTION_H
+#define ENCODE_INSTRUCTION_H
+
 #include <stdint.h>
 #include "parser.h"
 
 #define HALT_INSTRUCTION_BIG_ENDIAN 0x0000008A
 
 uint32_t encode_instruction(instruction *instr);
+
+#endif // ENCODE_INSTRUCTION_H
