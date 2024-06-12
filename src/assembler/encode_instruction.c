109cd721ee1880e0cde7b1dadabd21cecf628d83
Stefan van Biljon <sv823@gpu35.doc.ic.ac.uk>
2024-06-12 12:08:43 +0100

    initial encode files

---
+++ src/assembler/encode_instruction.c
@@ -0,0 +1,6 @@
+#include <stdint.h>
+#include "parser.h"
+
+uint32_t encode_instruction(&instr, table) {
+    return 0;
+}
---
+++ src/assembler/encode_instruction.h
@@ -0,0 +1,4 @@
+#include <stdint.h>
+#include "parser.h"
+
+uint32_t encode_instruction(&instr, table);
