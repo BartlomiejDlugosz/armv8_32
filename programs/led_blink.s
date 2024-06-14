1273af36fc18a0accb05f9d76396d2e3bb578dcf
Stefan van Biljon <sv823@gpu35.doc.ic.ac.uk>
2024-06-14 17:55:17 +0100

    working blinking led

--- programs/led_blink.s
+++ programs/led_blink.s
@@ -1,15 +1,20 @@
 ldr w21 gpio21
 ldr w22 setup_gpio
-str w21 [x22] 
-
-high:
+str w21 [x22]
+ldr w18 clear_gpio
+ldr w19 set_gpio
 ldr w20 bit21
-str w0 [x20]
-ldr wzr clear_gpio
-ldr w0 set_gpio
+
+ldr w17 zero
+ldr w28 zero
+ldr w29 zero
+
+set:
+str wzr [x18]
+str w20 [x19]
 
 delay:
-add x28, x28, #1000
+    add x28, x28, #10000
 delay_outer_loop:
     add x29, x29, #1000000
 delay_inner_loop:
@@ -18,24 +23,11 @@ delay_inner_loop:
 subs x28, x28, #1
 b.ne delay_outer_loop
 
-low:
-ldr w20 bit21
-str w0 [x20]
-ldr wzr set_gpio
-ldr w0 clear_gpio
+mov x17 x18
+mov x18 x19
+mov x19 x17
 
-delay:
-add x28, x28, #1000
-delay_outer_loop:
-    add x29, x29, #1000000
-delay_inner_loop:
-    subs x29, x29, #1
-    b.ne delay_inner_loop
-subs x28, x28, #1
-b.ne delay_outer_loop
-
-and x0, x0, x0
-b high
+b set
 
 setup_gpio:
  .int 0x3f200008
@@ -47,3 +39,5 @@ gpio21:
   .int 0x8
 bit21:
   .int 0x200000
+zero:
+  .int 0x0
