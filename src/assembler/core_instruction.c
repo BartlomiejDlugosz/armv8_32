bf1d676db30d5ab0c67cf6f96a9d449194dab2f1
Stefan van Biljon <sv823@gpu35.doc.ic.ac.uk>
2024-06-14 21:49:49 +0100

    added core_instruction

---
+++ src/assembler/core_instruction.c
@@ -0,0 +1,16 @@
+#include "parser.h"
+#include "dynamicString.h"
+#include "core_instruction.h"
+
+core_instruction *generate_core_instruction(instruction *instr) {
+    core_instruction *new_cinstr;
+
+    new_cinstr->opcode = instr->opcode;
+    new_cinstr->operand0 = getString(instr->operand[0]);
+    new_cinstr->operand1 = getString(instr->operand[1]);
+    new_cinstr->operand2 = getString(instr->operand[2]);
+    new_cinstr->operand3 = getString(instr->operand[3]);
+    new_cinstr->operand4 = getString(instr->operand[4]);
+
+    return new_cinstr;
+}
---
+++ src/assembler/core_instruction.h
@@ -0,0 +1,17 @@
+#ifndef CORE_INSTRUCTION_H
+#define CORE_INSTRUCTION_H
+
+#include "parser.h"
+
+typedef struct {    
+    const char *opcode;
+    const char *operand0;
+    const char *operand1;
+    const char *operand2;
+    const char *operand3;
+    const char *operand4;
+} core_instruction;
+
+core_instruction *generate_core_instruction(instruction *instr);
+
+#endif // CORE_INSTRUCTION_H
