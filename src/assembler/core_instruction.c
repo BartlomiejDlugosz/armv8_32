fb3d1a0a4a01edc5027a64d0536add2c6640f142
Stefan van Biljon <sv823@gpu35.doc.ic.ac.uk>
2024-06-14 21:57:03 +0100

    typo in operand, and remove const

--- src/assembler/core_instruction.c
+++ src/assembler/core_instruction.c
@@ -6,11 +6,11 @@ core_instruction *generate_core_instruction(instruction *instr) {
     core_instruction *new_cinstr;
 
     new_cinstr->opcode = instr->opcode;
-    new_cinstr->operand0 = getString(instr->operand[0]);
-    new_cinstr->operand1 = getString(instr->operand[1]);
-    new_cinstr->operand2 = getString(instr->operand[2]);
-    new_cinstr->operand3 = getString(instr->operand[3]);
-    new_cinstr->operand4 = getString(instr->operand[4]);
+    new_cinstr->operand0 = getString(instr->operands[0]);
+    new_cinstr->operand1 = getString(instr->operands[1]);
+    new_cinstr->operand2 = getString(instr->operands[2]);
+    new_cinstr->operand3 = getString(instr->operands[3]);
+    new_cinstr->operand4 = getString(instr->operands[4]);
     new_cinstr->line_number = instr->line_number;
 
     return new_cinstr;
