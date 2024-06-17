b41863968c3655c887fe26537bc530c1f3fb1298
Stefan van Biljon <sv823@gpu35.doc.ic.ac.uk>
2024-06-18 00:03:44 +0100

    compiles and add road.c

--- extension2/road.h
+++ extension2/road.h
@@ -13,19 +13,3 @@ typedef struct {
 // head is returned so use as
 // road->first_car = maybe_add_cars(road, f(x));
 car *maybe_add_cars(road, random_function);
-
-
-/*
- change_function = &minimax;
-while (true) { // timestep
-    update_lights_to_next_state(change_function) // hmm, is it time to update? minimax
-
-    for (each road) {
-        update_distances() // pop off ANY cars which have passed stop line
-        remove_crossed() // pop off ANY cars which have passed stop line
-        maybe_add_cars() // random function + also check sum < max_length
-    }
-}
-
-*/
-
--- extension2/simulate_traffic.c
+++ extension2/simulate_traffic.c
@@ -1,4 +1,21 @@
 int main(int argc, char **argv) {
 
-
+    return 0;
 }
+
+
+
+
+/*
+ change_function = &minimax;
+while (true) { // timestep
+    update_lights_to_next_state(change_function) // hmm, is it time to     update? minimax
+
+    for (each road) {
+        update_distances() // pop off ANY cars which have passed stop l    ine
+        remove_crossed() // pop off ANY cars which have passed stop lin    e
+        maybe_add_cars() // random function + also check sum < max_leng    th
+    }
+}
+
+*/
