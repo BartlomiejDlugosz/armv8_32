430991f691ed307c97d23f6d101dac1ac1ad80ef
Stefan van Biljon <sv823@gpu35.doc.ic.ac.uk>
2024-06-18 20:57:46 +0100

    added randomness in calling maybe_add_cars

---
+++ extension2/road.c
@@ -0,0 +1,19 @@
+#include "traffic_light.h"
+#include "car.h"
+
+/*
+typedef struct {
+    int length; // traverse the ll, only adding if sum < length
+    int speed_limit;
+    car *head_car;
+    traffic_light light;
+} road;
+*/
+
+// think about case where road->first_car == NULL
+// in the case where nothing is updated, the previous
+// head is returned so use as
+// road->first_car = maybe_add_cars(road, f(x));
+car *maybe_add_cars() {
+
+}
