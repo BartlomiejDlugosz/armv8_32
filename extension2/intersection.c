2d08e5808e238cac3821baecb69a590d678330d9
Zeshan Uddin <zu123@ic.ac.uk>
2024-06-18 20:59:59 +0100

    started working on updating the lights

--- extension2/intersection.c
+++ extension2/intersection.c
@@ -2,12 +2,15 @@
 #include "intersection.h"
 #include <time.h>
 #include "states.h"
+#include "strategies.h"
 /*
 bool update_lights_to_next_state(strategy *s) {
     return true;
 }
 */
 
-bool update_lights_to_next_state(intersection isec, time_t dt, time_t time_since_change) {
+bool update_lights_to_next_state(intersection *isec, time_t dt, time_t *time_since_change, strategy *s) {
+    if ((*s) (isec, time_since_change)) {
 
+    }
 }
