c14342ac4131e9ab3bbec11fb9e1b5dbd2d452bd
ishaan <ishaan@DESKTOP-N2FKMMP>
2024-06-18 22:46:38 +0100

    hopefully written logically correct code for update distances in road..c

--- extension2/road.c
+++ extension2/road.c
@@ -9,12 +9,39 @@ void *update_distances(road* update_road, time_t dt)
 {   
     int speed_limit = update_road->speed_limit;
     // the lights can be checked using the road (it has a light attribute)
+    int distance_covered = speed_limit * dt;
     if (update_road->light.clr == RED) {
         // dealing with HEAD
         int cur_distance = update_road->head_car->distance_to_car_in_front; // distance to light
         // distance = speed * time
-        int new_distance = cur_distance - (speed_limit * dt) < 0;
+        int new_distance = cur_distance - distance_covered;
         update_road->head_car->distance_to_car_in_front = MAX(new_distance, 0);
+        bool all_cars_in_front_are_stationary = false;
+        if (cur_distance == new_distance) {
+            all_cars_in_front_are_stationary = true;
+        }
+        distance_covered -= cur_distance - new_distance;
+        // Dealing with rest of the cars
+        car* prev_car = update_road->head_car;
+        car* next_car = update_road->head_car->next;
+        int follow_distance = update_road->follow_distance; // minimum separation needed between cars
+        while (next_car != NULL && distance_covered != 0) {
+
+            int distance_to_car_in_front_prev = next_car->distance_to_car_in_front; // previous 
+            next_car->distance_to_car_in_front = MAX(follow_distance, next_car->distance_to_car_in_front - distance_covered);
+            distance_covered -= distance_to_car_in_front_prev - next_car->distance_to_car_in_front;
+            if (next_car->distance_to_car_in_front == distance_to_car_in_front_prev && all_cars_in_front_are_stationary) {
+                next_car->time_stationary += dt;
+            } else {
+                all_cars_in_front_are_stationary = false;
+            }
+
+            next_car = next_car->next;
+
+        }
+
+    } else if (update_road->light.clr == GREEN) {
+        update_road->head_car->distance_to_car_in_front -= distance_covered; //distance to light can now become negative
     }
 }
 
