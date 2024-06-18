8ded03e1bdbe81a769710a12cb0742c22d17531e
Ishaan <ishaan@IshaanVyas>
2024-06-18 17:46:06 +0100

    add stationary time to car struct

--- extension2/car.h
+++ extension2/car.h
@@ -1,21 +1,26 @@
 #ifndef CAR_H
 #define CAR_H
 
+#include <time.h>
 #include "road.h"
 
 typedef struct car {
     int distance_to_car_in_front; // distance_to_light if HEAD
     struct car *next; // possibly null
+    time_t time_stationary; // time stationary initially 0
 } car;
+
+// need to keep track how long they are waiting for
 
 
 // check lights (just once)
 // update distance of all cars (starting with first)
+// if distance stays the same, add time to stationary time
 void *update_distances();
 
-// pop off ANY cars which have passed stop line, that involves
-// potentially popping many cars, updating the running head
-// return head
-car *remove_crossed();
+// pop off ANY cars which have passed stop line (checking for negative distances), that involves
+// potentially popping many cars, updating the running head and its distance to the light
+// return the head of number of cars crossed
+car* remove_crossed();
 
 #endif // CAR_H
