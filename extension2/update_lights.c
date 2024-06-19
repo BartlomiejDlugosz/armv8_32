fce48e04ab3c64c56cf456c7eeef48617abd7f03
Zeshan Uddin <zu123@ic.ac.uk>
2024-06-19 01:01:38 +0100

    fixed update lights arguments and simulate traffic

--- extension2/update_lights.c
+++ extension2/update_lights.c
@@ -4,8 +4,8 @@
 
 #include "update_lights.h"
 
-bool update_lights_to_next_state(intersection *isec, time_t dt, time_t *time_since_change, strategy *s) {
-    if ((*s) (isec, time_since_change)) {
+bool update_lights_to_next_state(intersection *isec, time_t dt, time_t *time_since_change, strategy s) {
+    if ((s) (isec, time_since_change)) {
         isec->state_index = (isec->state_index + 1) % (NUM_STATES + 1);
         for(int i = 0; i < NUM_ROADS; i++) {
             printf("Road: %i , CLR: %d to ", i, isec->lights[i].clr);
