b7743f3555a39c4518f28fc0deed6f85ceb0598c
Stefan van Biljon <sv823@gpu35.doc.ic.ac.uk>
2024-06-21 12:18:11 +0100

    added automatic plotting of all strategies using make graphs

--- extension2/graphing/main.py
+++ extension2/graphing/main.py
@@ -1,24 +1,28 @@
 from functions import *
+import numpy as np
 
-strategy = "Genetic Algorithm Avg-road"
+names = ["Genetic Algorithm Max", "Genetic Algorithm Avg", "Basic", "Basic Plus"]
 
-yss = [genxy(file) for file in [ strategy + '0.txt', strategy + '1.txt', strategy + '2.txt', strategy + '3.txt' ]]
-xs = np.linspace(0, len(yss[0]), len(yss[0]), endpoint=False)
+for i in range(len(names)):
+    name = names[i]
+    strategy = name + "-road"
 
-ys_isec = yss[0]
-for i in range(1, len(yss)):
-    for j in range(0, len(yss[i])):
-        ys_isec[j] += yss[i][j]
+    yss = [genxy(file) for file in [ strategy + '0.txt', strategy + '1.txt', strategy + '2.txt', strategy + '3.txt' ]]
+    xs = np.linspace(0, len(yss[0]), len(yss[0]), endpoint=False)
 
-for i in range(len(yss)):
-    ys = yss[i]
+    ys_isec = yss[0]
+    for i in range(1, len(yss)):
+        ys_isec = np.add(ys_isec, yss[i])
 
-    initalisePlot("Cars on road" + str(i) + "over time ",
-                  xs, ys, '$t$ / s', "num cars")
-    savePlot("result" + str(i))
+    for i in range(len(yss)):
+        ys = yss[i]
+
+        initalisePlot(name + ": Cars on road " + str(i) + " over time ",
+                      xs, ys, '$t$ / s', "num cars")
+        savePlot("result" + str(i) + strategy)
 
 
-initalisePlot("Total cars in intersection over time ",
-              xs, ys_isec, '$t$ / s', "num cars")
-savePlot("total_result")
+    initalisePlot(name + ": Total cars in intersection over time ",
+                  xs, ys_isec, '$t$ / s', "num cars")
+    savePlot("total_result" + strategy)
 
